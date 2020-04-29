ae7b0777d38ad1c3d3c872c40fe593ef2390ea78
happycastle114 <41810556+happycastle114@users.noreply.github.com>
2020-04-29 23:45:20 +0900

    Fix Router to HashRouter

--- precache-manifest.ee89f0bfa614847948d92c062b4534f4.js
+++ precache-manifest.ee89f0bfa614847948d92c062b4534f4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f242cf07b8ede13cb3324570556b48f9",
+    "revision": "e0434cc03c4c128c39d3078570333e61",
     "url": "/index.html"
   },
   {
-    "revision": "0263c64cf466099ac029",
+    "revision": "99ce0dd4c8f22523afe1",
     "url": "/static/css/2.42d2bd67.chunk.css"
   },
   {
-    "revision": "5c821cb4d8d79949b5ec",
+    "revision": "a3cf651ca45cbf9c9627",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "0263c64cf466099ac029",
-    "url": "/static/js/2.be980861.chunk.js"
+    "revision": "99ce0dd4c8f22523afe1",
+    "url": "/static/js/2.d677e3e1.chunk.js"
   },
   {
     "revision": "ef0f859796a3d2d5a811c44ab7864144",
-    "url": "/static/js/2.be980861.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.d677e3e1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5c821cb4d8d79949b5ec",
-    "url": "/static/js/main.353598fd.chunk.js"
+    "revision": "a3cf651ca45cbf9c9627",
+    "url": "/static/js/main.16c3992a.chunk.js"
   },
   {
     "revision": "5795f75938eb08dc1f25",
